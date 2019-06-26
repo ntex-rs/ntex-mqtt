886b57b0a1f394e2cda70aa0a74563ad94198228
Nikolay Kim <fafhrd91@gmail.com>
2019-06-26 15:36:27 +0600

    add basic client test

--- tests/test_server.rs
+++ tests/test_server.rs
@@ -1,6 +1,11 @@
+use actix_ioframe as ioframe;
+use actix_service::Service;
 use actix_test_server::TestServer;
+use bytes::Bytes;
 use futures::future::ok;
-use futures::Future;
+use futures::{Future, Sink};
+use mqtt_codec as mqtt;
+use string::TryFrom;
 
 use actix_mqtt::{Connect, ConnectAck, MqttServer};
 
@@ -19,7 +24,44 @@ fn test_simple() -> std::io::Result<()> {
     );
     env_logger::init();
 
-    let _srv = TestServer::with(|| MqttServer::new(connect));
+    let mut srv = TestServer::with(|| MqttServer::new(connect));
+
+    struct ClientSession;
+
+    let mut client = ioframe::Builder::new()
+        .service(|conn: ioframe::Connect<_>| {
+            // construct connect result
+            let result = conn
+                .codec(mqtt::Codec::new())
+                .state(ClientSession)
+                .into_result();
+
+            // send Connect packet
+            result
+                .send(mqtt::Packet::Connect(mqtt::Connect {
+                    protocol: mqtt::Protocol::default(),
+                    clean_session: true,
+                    keep_alive: 10,
+                    last_will: None,
+                    client_id: string::String::try_from(Bytes::from_static(b"user")).unwrap(),
+                    username: None,
+                    password: None,
+                }))
+                .map_err(|_| panic!())
+        })
+        .finish(|t: ioframe::Item<_, mqtt::Codec>| {
+            t.sink().close();
+            Ok(None)
+        });
+
+    let conn = srv
+        .block_on(
+            actix_connect::default_connector()
+                .call(actix_connect::Connect::with(String::new(), srv.addr())),
+        )
+        .unwrap();
+
+    srv.block_on(client.call(conn.into_parts().0)).unwrap();
 
     Ok(())
 }
