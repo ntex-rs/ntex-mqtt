4642edabc92aa15ea397bb3bd2bbeb4d9d869744
Nikolay Kim <fafhrd91@gmail.com>
2020-03-30 00:52:42 +0600

    expose read-only state

--- src/session.rs
+++ src/session.rs
@@ -1,11 +1,10 @@
-use std::cell::{Ref, RefCell, RefMut};
 use std::rc::Rc;
 use std::time::Duration;
 
 use crate::sink::MqttSink;
 
 /// Mqtt connection session
-pub struct Session<St>(Rc<RefCell<SessionInner<St>>>);
+pub struct Session<St>(Rc<SessionInner<St>>);
 
 struct SessionInner<St> {
     st: St,
@@ -22,24 +21,24 @@ impl<St> Clone for Session<St> {
 
 impl<St> Session<St> {
     pub(crate) fn new(st: St, sink: MqttSink, timeout: Duration, in_flight: usize) -> Self {
-        Session(Rc::new(RefCell::new(SessionInner {
+        Session(Rc::new(SessionInner {
             st,
             sink,
             timeout,
             in_flight,
-        })))
+        }))
     }
 
-    pub fn sink(&self) -> Ref<'_, MqttSink> {
-        Ref::map(self.0.borrow(), |inner| &inner.sink)
+    pub fn sink(&self) -> &MqttSink {
+        &self.0.sink
     }
 
-    pub fn state(&self) -> RefMut<'_, St> {
-        RefMut::map(self.0.borrow_mut(), |inner| &mut inner.st)
+    pub fn state(&self) -> &St {
+        &self.0.st
     }
 
     pub(super) fn params(&self) -> (Duration, usize) {
-        let inner = self.0.borrow();
+        let inner = self.0.as_ref();
         (inner.timeout, inner.in_flight)
     }
 }
