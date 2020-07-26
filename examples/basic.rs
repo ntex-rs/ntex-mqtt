110c3646aa8ad4504b901b1cb6bf37403c5a4a56
Nikolay Kim <fafhrd91@gmail.com>
2020-07-26 17:10:35 +0600

    use both v3 and v5 servers for basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,20 +1,55 @@
-use ntex_mqtt::v3::{Connect, ConnectAck, MqttServer, Publish};
+use ntex_mqtt::{v3, v5, MqttServer};
 
 #[derive(Clone)]
 struct Session;
 
-async fn connect<Io>(connect: Connect<Io>) -> Result<ConnectAck<Io, Session>, ()> {
+#[derive(Debug)]
+struct ServerError;
+
+impl From<()> for ServerError {
+    fn from(_: ()) -> Self {
+        ServerError
+    }
+}
+
+impl std::convert::TryFrom<ServerError> for v5::PublishAck {
+    type Error = ServerError;
+
+    fn try_from(err: ServerError) -> Result<Self, Self::Error> {
+        Err(err)
+    }
+}
+
+async fn connect_v3<Io>(
+    connect: v3::Connect<Io>,
+) -> Result<v3::ConnectAck<Io, Session>, ServerError> {
     log::info!("new connection: {:?}", connect);
     Ok(connect.ack(Session, false))
 }
 
-async fn publish(publish: Publish) -> Result<(), ()> {
+async fn publish_v3(publish: v3::Publish) -> Result<(), ServerError> {
     log::info!(
         "incoming publish: {:?} -> {:?}",
         publish.id(),
         publish.topic()
     );
     Ok(())
+}
+
+async fn connect_v5<Io>(
+    connect: v5::Connect<Io>,
+) -> Result<v5::ConnectAck<Io, Session>, ServerError> {
+    log::info!("new connection: {:?}", connect);
+    Ok(connect.ack(Session))
+}
+
+async fn publish_v5(publish: v5::Publish) -> Result<v5::PublishAck, ServerError> {
+    log::info!(
+        "incoming publish: {:?} -> {:?}",
+        publish.id(),
+        publish.topic()
+    );
+    Ok(publish.ack())
 }
 
 #[ntex::main]
@@ -24,7 +59,9 @@ async fn main() -> std::io::Result<()> {
 
     ntex::server::Server::build()
         .bind("mqtt", "127.0.0.1:1883", || {
-            MqttServer::new(connect).publish(publish).finish()
+            MqttServer::new()
+                .v3(v3::MqttServer::new(connect_v3).publish(publish_v3))
+                .v5(v5::MqttServer::new(connect_v5).publish(publish_v5))
         })?
         .workers(1)
         .run()
