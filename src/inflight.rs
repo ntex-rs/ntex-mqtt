703adfa447f944240e5f7217640116fd4677966e
Nikolay Kim <fafhrd91@gmail.com>
2022-12-28 13:12:26 +0100

    wip

--- src/inflight.rs
+++ src/inflight.rs
@@ -191,7 +191,7 @@ mod tests {
 
         let _ = res.await;
         assert_eq!(lazy(|cx| srv.poll_ready(cx)).await, Poll::Ready(Ok(())));
-        assert!(lazy(|cx| srv.poll_shutdown(cx, false)).await.is_ready());
+        assert!(lazy(|cx| srv.poll_shutdown(cx)).await.is_ready());
     }
 
     #[ntex::test]
