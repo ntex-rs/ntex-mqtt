316fdb9a894be4348e1b74c3801a50caffde5654
Nikolay Kim <fafhrd91@gmail.com>
2024-11-04 19:43:33 +0500

    wip

--- src/io.rs
+++ src/io.rs
@@ -646,6 +646,7 @@ mod tests {
                         service: Pipeline::new(service.into_service()).bind(),
                         response: None,
                         response_idx: 0,
+                        ready_count: 0,
                         io: IoBoxed::from(io),
                         st: IoDispatcherState::Processing,
                         flags: if keepalive_timeout.is_zero() {
